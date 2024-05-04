e05c1afe496db3f62824128dadfbb58f872317bd
EdQuinx <bjnboo833626@gmail.com>
2024-05-05 05:40:41 +0700

    first commit

---
+++ src/theme.js
@@ -0,0 +1,35 @@
+// import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+
+const APP_BAR_HEIGHT = '58px'
+const BOARD_BAR_HEIGHT = '60px'
+const BOARD_CONTENT_HEIGHT = `calc(100vh - ${APP_BAR_HEIGHT} - ${BOARD_BAR_HEIGHT})`
+
+// Create a theme instance.
+const theme = extendTheme({
+  trello: {
+    appBarHeight: APP_BAR_HEIGHT,
+    boardBarHeight: BOARD_BAR_HEIGHT,
+    boardContentHeight: BOARD_CONTENT_HEIGHT
+  },
+  colorSchemes: {
+    // light: {
+    //   palette: {
+    //     primary: teal,
+    //     secondary: deepOrange
+    //   }
+    // },
+    // dark: {
+    //   palette: {
+    //     primary: cyan,
+    //     secondary: orange
+    //   }
+    // }
+  },
+  components: {
+  }
+  // ...other properties
+})
+
+
+export default theme
